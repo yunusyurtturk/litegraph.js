a45000786bd79c69e092a74457c77146e04b254f
Daniel Lewis <daniel.lewis@plailabs.com>
2024-05-07 21:17:20 -0600

    Improvements to DragAndScale

--- src/dragandscale.js
+++ src/dragandscale.js
@@ -2,7 +2,9 @@ import { LiteGraph } from "./litegraph.js";
 
 //Scale and Offset
 export class DragAndScale {
+
     constructor(element, skip_events) {
+
         this.offset = new Float32Array([0, 0]);
         this.scale = 1;
         this.max_scale = 10;
@@ -23,19 +25,79 @@ export class DragAndScale {
 
     bindEvents(element) {
         this.last_mouse = new Float32Array(2);
+        element.addEventListener("mousedown", this.onMouseDown);
+        element.addEventListener("wheel", this.onWheel);
+    }
 
-        this._binded_mouse_callback = this.onMouse.bind(this);
+    onMouseDown = (e) => {
+        if (!this.enabled) {
+            return;
+        }
 
-        LiteGraph.pointerListenerAdd(element,"down", this._binded_mouse_callback);
-        LiteGraph.pointerListenerAdd(element,"move", this._binded_mouse_callback);
-        LiteGraph.pointerListenerAdd(element,"up", this._binded_mouse_callback);
+        const canvas = this.element;
+        const rect = canvas.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
+        e.canvasx = x;
+        e.canvasy = y;
+        e.dragging = this.dragging;
+        
+        var is_inside = !this.viewport || ( this.viewport && x >= this.viewport[0] && x < (this.viewport[0] + this.viewport[2]) && y >= this.viewport[1] && y < (this.viewport[1] + this.viewport[3]) );
 
-        element.addEventListener(
-            "mousewheel",
-            this._binded_mouse_callback,
-            false
-        );
-        element.addEventListener("wheel", this._binded_mouse_callback, false);
+        if (is_inside) {
+            this.dragging = true;
+            this.abortController = new AbortController();
+            document.addEventListener("mousemove",this.onMouseMove, { signal: this.abortController.signal });
+            document.addEventListener("mouseup",this.onMouseUp, { signal: this.abortController.signal });
+        } 
+
+        this.last_mouse[0] = x;
+        this.last_mouse[1] = y;
+
+    }
+
+    onMouseMove = (e) => {
+        if (!this.enabled) {
+            return;
+        }
+
+        const canvas = this.element;
+        const rect = canvas.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
+        e.canvasx = x;
+        e.canvasy = y;
+        e.dragging = this.dragging;
+        
+        var is_inside = !this.viewport || ( this.viewport && x >= this.viewport[0] && x < (this.viewport[0] + this.viewport[2]) && y >= this.viewport[1] && y < (this.viewport[1] + this.viewport[3]) );
+
+
+        var deltax = x - this.last_mouse[0];
+        var deltay = y - this.last_mouse[1];
+        if (this.dragging) {
+            this.mouseDrag(deltax, deltay);
+        }
+
+        this.last_mouse[0] = x;
+        this.last_mouse[1] = y;
+
+    }
+
+    onMouseUp = (event) => {
+        this.dragging = false;
+        this.abortController?.abort();
+    }
+
+    onWheel = (event) => {
+        event.wheel = -event.deltaY;
+            
+        //from stack overflow
+        event.delta = event.wheelDelta
+            ? event.wheelDelta / 40
+            : event.deltaY
+            ? -event.deltaY / 3
+            : 0;
+        this.changeDeltaScale(1.0 + event.delta * 0.05);
     }
 
     computeVisibleArea(viewport) {
@@ -62,79 +124,6 @@ export class DragAndScale {
         this.visible_area[3] = endy - starty;
     }
 
-    onMouse(e) {
-        if (!this.enabled) {
-            return;
-        }
-
-        var canvas = this.element;
-        var rect = canvas.getBoundingClientRect();
-        var x = e.clientX - rect.left;
-        var y = e.clientY - rect.top;
-        e.canvasx = x;
-        e.canvasy = y;
-        e.dragging = this.dragging;
-        
-        var is_inside = !this.viewport || ( this.viewport && x >= this.viewport[0] && x < (this.viewport[0] + this.viewport[2]) && y >= this.viewport[1] && y < (this.viewport[1] + this.viewport[3]) );
-
-        //console.log("pointerevents: DragAndScale onMouse "+e.type+" "+is_inside);
-        
-        var ignore = false;
-        if (this.onmouse) {
-            ignore = this.onmouse(e);
-        }
-
-        if (e.type == LiteGraph.pointerevents_method+"down" && is_inside) {
-            this.dragging = true;
-            LiteGraph.pointerListenerRemove(canvas,"move",this._binded_mouse_callback);
-            LiteGraph.pointerListenerAdd(document,"move",this._binded_mouse_callback);
-            LiteGraph.pointerListenerAdd(document,"up",this._binded_mouse_callback);
-        } else if (e.type == LiteGraph.pointerevents_method+"move") {
-            if (!ignore) {
-                var deltax = x - this.last_mouse[0];
-                var deltay = y - this.last_mouse[1];
-                if (this.dragging) {
-                    this.mouseDrag(deltax, deltay);
-                }
-            }
-        } else if (e.type == LiteGraph.pointerevents_method+"up") {
-            this.dragging = false;
-            LiteGraph.pointerListenerRemove(document,"move",this._binded_mouse_callback);
-            LiteGraph.pointerListenerRemove(document,"up",this._binded_mouse_callback);
-            LiteGraph.pointerListenerAdd(canvas,"move",this._binded_mouse_callback);
-        } else if ( is_inside &&
-            (e.type == "mousewheel" ||
-            e.type == "wheel" ||
-            e.type == "DOMMouseScroll")
-        ) {
-            e.eventType = "mousewheel";
-            if (e.type == "wheel") {
-                e.wheel = -e.deltaY;
-            } else {
-                e.wheel =
-                    e.wheelDeltaY != null ? e.wheelDeltaY : e.detail * -60;
-            }
-
-            //from stack overflow
-            e.delta = e.wheelDelta
-                ? e.wheelDelta / 40
-                : e.deltaY
-                ? -e.deltaY / 3
-                : 0;
-            this.changeDeltaScale(1.0 + e.delta * 0.05);
-        }
-
-        this.last_mouse[0] = x;
-        this.last_mouse[1] = y;
-
-        if(is_inside)
-        {
-            e.preventDefault();
-            e.stopPropagation();
-            return false;
-        }
-    }
-
     toCanvasContext(ctx) {
         ctx.scale(this.scale, this.scale);
         ctx.translate(this.offset[0], this.offset[1]);
@@ -158,27 +147,17 @@ export class DragAndScale {
         this.offset[0] += x / this.scale;
         this.offset[1] += y / this.scale;
 
-        if (this.onredraw) {
-            this.onredraw(this);
-        }
+        this.onredraw?.(this);
     }
 
     changeScale(value, zooming_center) {
-        if (value < this.min_scale) {
-            value = this.min_scale;
-        } else if (value > this.max_scale) {
-            value = this.max_scale;
-        }
 
-        if (value == this.scale) {
+        value = LiteGraph.clamp(value, this.min_scale, this.max_scale);
+        
+        if (value == this.scale || !this.element) {
             return;
         }
-
-        if (!this.element) {
-            return;
-        }
-
-        var rect = this.element.getBoundingClientRect();
+        const rect = this.element.getBoundingClientRect();
         if (!rect) {
             return;
         }
@@ -187,6 +166,7 @@ export class DragAndScale {
             rect.width * 0.5,
             rect.height * 0.5
         ];
+
         var center = this.convertCanvasToOffset(zooming_center);
         this.scale = value;
         if (Math.abs(this.scale - 1) < 0.01) {
@@ -202,9 +182,7 @@ export class DragAndScale {
         this.offset[0] += delta_offset[0];
         this.offset[1] += delta_offset[1];
 
-        if (this.onredraw) {
-            this.onredraw(this);
-        }
+        this.onredraw?.(this);
     }
 
     changeDeltaScale(value, zooming_center) {
