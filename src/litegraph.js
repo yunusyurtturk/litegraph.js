ebce35cee9385a4db6f217e9cd193d0077a9bd3f
atlasan <gmorittu@gmail.com>
2024-05-27 20:55:31 +0200

    CallbacksHandlers

--- src/litegraph.js
+++ src/litegraph.js
@@ -347,8 +347,10 @@ export var LiteGraph = new class {
             this.Nodes[classname] = base_class;
         }
         LiteGraph.onNodeTypeRegistered?.(type, base_class);
+        LiteGraph.processCallbackHandlers("onNodeTypeRegistered");
         if (prev) {
             LiteGraph.onNodeTypeReplaced?.(type, base_class, prev);
+            LiteGraph.processCallbackHandlers("onNodeTypeReplaced");
         }
 
         // warnings
@@ -1060,50 +1062,67 @@ export var LiteGraph = new class {
 
     // WIP
     // intended to replace direct (single) assignment of callbacks [ event entrypoint ]
-    registerCallbackHandler = (name, callback, opts) => {
+    registerCallbackHandler(name, callback, opts){
         if(!opts || typeof(opts)!=="object") opts = {};
         const def_opts = {priority: 0};
         opts = Object.assign(def_opts, opts);
         
-        if(typeof(this.callbacks_handlers[name]) === "undefined"){
-            this.callbacks_handlers[name] = {last_id: 0, handlers:{}};
+        if(typeof(callback)!=="function"){
+            LiteGraph.log_error("registerCallbackHandler","Invalid callback");
+            return false;
         }
-        const h_id = this.callbacks_handlers[name].last_id+1;
+
+        if(typeof(this.callbacks_handlers[name]) === "undefined"){
+            this.callbacks_handlers[name] = {last_id: 0, handlers:[]};
+        }
+        const h_id = this.callbacks_handlers[name].last_id++;
 
         LiteGraph.log_info("registerCallbackHandler","new callback",name,h_id);
 
-        this.callbacks_handlers[name].handlers[h_id] = {priority: opts.priority, callback: callback};
+        this.callbacks_handlers[name].handlers.push({id: h_id, priority: opts.priority, callback: callback});
 
         // sort descending
-        this.callbacks_handlers[name].handlers[h_id].sort((a, b) => b.priority - a.priority);
+        this.callbacks_handlers[name].handlers.sort((a, b) => b.priority - a.priority);
 
         LiteGraph.log_info("registerCallbackHandler","sorted",this.callbacks_handlers[name]);
 
         return h_id; // return the cbhandle id
     };
-    unregisterCallbackHandler = (name, h_id) => {
+    unregisterCallbackHandler(name, h_id){
         if(typeof(this.callbacks_handlers[name]) !== "undefined"){
-            if(typeof(this.callbacks_handlers[name][h_id]) !== "undefined"){
-                LiteGraph.log_info("unregisterCallbackHandler",name,h_id,this.callbacks_handlers[name]);
-                delete this.callbacks_handlers[name][h_id];
-                LiteGraph.log_info("unregisterCallbackHandler","removed",h_id,this.callbacks_handlers[name]);
+            const nHandlers = this.callbacks_handlers[name].handlers.length;
+            this.callbacks_handlers[name].handlers = this.callbacks_handlers[name].handlers.filter(function( obj ) {
+                if(obj.id === h_id){
+                    LiteGraph.log_info("unregisterCallbackHandler",name,h_id);
+                }
+                return obj.id !== h_id;
+            });
+            if(this.callbacks_handlers[name].handlers.length < nHandlers){
                 return true;
             }
         }
         return false;
     };
-    processCallbackHandlers = (name,opts) => {
+    processCallbackHandlers(name,opts){
         if(!opts || typeof(opts)!=="object") opts = {};
         const def_opts = {
             // WIP :: think try and setup
-            // return: "first", skip_null_return: true, append_last_return: false
+            // process: "all", return: "first", skip_null_return: true, append_last_return: false
         };
         opts = Object.assign(def_opts, opts);
 
         if(typeof(this.callbacks_handlers[name]) !== "undefined"){
-            for(let cbhX of this.callbacks_handlers[name]){
-                LiteGraph.log_info("processCallbackHandlers",name,cbhX);
-                cbhX(...(arguments.slice(2)));
+            function clean_args(args) {
+                let aRet = [];
+                for(let iA=0; iA<args.length; iA++) {
+                    if(typeof(args[iA])!=="undefined") aRet.push(args[iA]);
+                }
+                return aRet;
+            }
+            for(let cbhX of this.callbacks_handlers[name].handlers){
+                // LiteGraph.log_info("processCallbackHandlers",name,cbhX);
+                console.debug("processCallbackHandlers",name,cbhX);
+                cbhX.callback(...(clean_args(arguments).slice(2)));
             }
         }
     };
